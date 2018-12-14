724801468fed55dfe650970e11155a066d8de5fb
yeissonvenencia <yeissonvenencia@gmail.com>
2018-12-14 12:49:40 -0400

    taller IS

---
+++ src/TallerIS/IService1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TallerIS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "api/getData")]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "api/consultarPersonas")]
+        RespuestaLecturaPersonas ConsultarPersonas();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "api/consultarProfesores")]
+        RespuestaLecturaProfesores ConsultarProfesores();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "api/consultarDepartamentos")]
+        RespuestaLecturaDepartamentos ConsultarDepartamentos();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "api/agregarDepartamento")]
+        RespuestaEscritura AgregarDepartamento(String nombre, float creditos, int curso, int cuatrimestre, int idGrado, int idProfesor, int idTipoAsignatura);
+        // TODO: Add your service operations here
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "api/modificarDepartamento")]
+        RespuestaEscritura ModificarDepartamento(int idDepartamento, string nuevo);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "api/eliminarDepartamento")]
+        RespuestaEscritura EliminarDepartamento(int idDepartamento);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+    
+    [DataContract]
+    public abstract class Entidad { }
+
+    [DataContract]
+    public class Persona : Entidad {
+        string nombre;
+        string apellido;
+        string nif;
+        string fechaNac;
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        [DataMember]
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        [DataMember]
+        public string Nif
+        {
+            get { return nif; }
+            set { nif = value; }
+        }
+
+        [DataMember]
+        public string FechaNac
+        {
+            get { return fechaNac; }
+            set { fechaNac = value; }
+        }
+    }
+
+    [DataContract]
+    public class Departamento : Entidad
+    {
+        int id;
+        string nombre;
+
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+
+    [DataContract]
+    public class Profesores : Entidad
+    {
+        int id;
+        string nombre;
+        string asignatura;
+
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        [DataMember]
+        public string Asignatura
+        {
+            get { return asignatura; }
+            set { asignatura = value; }
+        }
+    }
+
+    [DataContract]
+    public class ListaPersonas : Entidad {
+        List<Persona> lista;
+
+        public ListaPersonas() {
+            lista = new List<Persona>();
+        }
+        
+        public void AgregarPersona(Persona persona) {
+            this.lista.Add(persona);
+        }
+
+        [DataMember]
+        public List<Persona> Lista
+        {
+            get { return lista; }
+            set { lista = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class ListaDepartamentos : Entidad
+    {
+        List<Departamento> lista;
+
+        public ListaDepartamentos()
+        {
+            lista = new List<Departamento>();
+        }
+
+        public void AgregarDepartamento(Departamento departamento)
+        {
+            this.lista.Add(departamento);
+        }
+
+        [DataMember]
+        public List<Departamento> Lista
+        {
+            get { return lista; }
+            set { lista = value; }
+        }
+
+    }
+
+    [DataContract]
+    public abstract class RespuestaGenerica{
+        int status;
+        string mensaje;
+
+        [DataMember]
+        public int Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        [DataMember]
+        public string Mensaje
+        {
+            get { return mensaje; }
+            set { mensaje = value; }
+        }
+    }
+
+    [DataContract]
+    public class RespuestaLecturaPersonas : RespuestaGenerica {
+
+        ListaPersonas listado;
+
+        [DataMember]
+        public ListaPersonas Listado
+        {
+            get { return listado; }
+            set { listado = value; }
+        }
+    }
+
+    [DataContract]
+    public class RespuestaLecturaDepartamentos : RespuestaGenerica
+    {
+
+        ListaDepartamentos listado;
+
+        [DataMember]
+        public ListaDepartamentos Listado
+        {
+            get { return listado; }
+            set { listado = value; }
+        }
+    }
+
+    [DataContract]
+    public class ListaProfesores : Entidad
+    {
+        List<Profesores> lista;
+
+        public ListaProfesores()
+        {
+            lista = new List<Profesores>();
+        }
+
+        public void AgregarProfesor(Profesores profesor)
+        {
+            this.lista.Add(profesor);
+        }
+
+        [DataMember]
+        public List<Profesores> Lista
+        {
+            get { return lista; }
+            set { lista = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class RespuestaLecturaProfesores : RespuestaGenerica
+    {
+
+        ListaProfesores listado;
+
+        [DataMember]
+        public ListaProfesores Listado
+        {
+            get { return listado; }
+            set { listado = value; }
+        }
+    }
+
+    [DataContract]
+    public class RespuestaEscritura : RespuestaGenerica
+    {
+        int afectados;
+
+        [DataMember]
+        public int Afectados
+        {
+            get { return afectados; }
+            set { afectados = value; }
+        }
+    }
+}
---
+++ src/TallerIS/Service1.svc.cs
@@ -0,0 +1,298 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TallerIS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    // System.Data.SqlClient
+    public class Service1 : IService1
+    {
+        static readonly log4net.ILog log = 
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public RespuestaLecturaPersonas ConsultarPersonas()
+        {
+            RespuestaLecturaPersonas respuesta = new RespuestaLecturaPersonas();
+            MySqlConnection conector =  null;
+            ListaPersonas lista = null;
+            try
+            {
+                log.Debug("Entrando al metodo: "+ MethodBase.GetCurrentMethod().Name);
+                conector = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString);
+                conector.Open();
+                MySqlCommand comandoSql = conector.CreateCommand();
+                comandoSql.CommandText = string.Format("SELECT nombre, apellido1, nif, fecha_nacimiento FROM persona;");
+                MySqlDataReader lectorTablaSQL = comandoSql.ExecuteReader();
+                lista = new ListaPersonas();
+                Persona persona = null;
+                while (lectorTablaSQL.Read())
+                {
+                    persona = new Persona();
+                    persona.Nombre = lectorTablaSQL.GetString(0);
+                    persona.Apellido = lectorTablaSQL.GetString(1);
+                    persona.Nif = lectorTablaSQL.GetString(2);
+                    persona.FechaNac = lectorTablaSQL.GetString(3);
+                    lista.AgregarPersona(persona);
+                }
+                respuesta.Status = 200;
+                respuesta.Mensaje = "Lectura exitosa";
+                respuesta.Listado=lista;                  
+            }
+            catch (Exception e)
+            {
+                log.Error("Error en la conexion a base de datos", e);
+                respuesta.Status = 500;
+                respuesta.Mensaje = "Error de conexion a la base de datos";
+                respuesta.Listado = null;
+                return respuesta;
+            }
+            finally
+            {
+                if (conector != null) {
+                    conector.Close();
+                    conector.Dispose();
+                }
+            }
+            log.Debug("Saliendo del metodo: " + MethodBase.GetCurrentMethod().Name);
+            return respuesta;
+        }
+
+        public RespuestaLecturaDepartamentos ConsultarDepartamentos()
+        {
+            RespuestaLecturaDepartamentos respuesta = new RespuestaLecturaDepartamentos();
+            MySqlConnection conector = null;
+            ListaDepartamentos lista = null;
+            try
+            {
+                log.Debug("Entrando al metodo: " + MethodBase.GetCurrentMethod().Name);
+                conector = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString);
+                conector.Open();
+                MySqlCommand comandoSql = conector.CreateCommand();
+                comandoSql.CommandText = string.Format("SELECT * FROM departamento;");
+                MySqlDataReader lectorTablaSQL = comandoSql.ExecuteReader();
+                lista = new ListaDepartamentos();
+                Departamento departamento = null;
+                while (lectorTablaSQL.Read())
+                {
+                    departamento = new Departamento();
+                    departamento.Id = Convert.ToInt32(lectorTablaSQL.GetString(0));
+                    departamento.Nombre = lectorTablaSQL.GetString(1);
+                    lista.AgregarDepartamento(departamento);
+                }
+                respuesta.Status = 200;
+                respuesta.Mensaje = "Lectura exitosa";
+                respuesta.Listado = lista;
+            }
+            catch (Exception e)
+            {
+                log.Error("Error en la conexion a base de datos", e);
+                respuesta.Status = 500;
+                respuesta.Mensaje = "Error de conexion a la base de datos";
+                respuesta.Listado = null;
+                return respuesta;
+            }
+            finally
+            {
+                if (conector != null)
+                {
+                    conector.Close();
+                    conector.Dispose();
+                }
+            }
+            log.Debug("Saliendo del metodo: " + MethodBase.GetCurrentMethod().Name);
+            return respuesta;
+        }
+
+        public RespuestaEscritura AgregarDepartamento(String nombre, float creditos, int curso, int cuatrimestre, int idGrado, int idProfesor, int idTipoAsignatura)
+        {
+            RespuestaEscritura respuesta = new RespuestaEscritura();
+            MySqlConnection conector = null;
+            try
+            {
+                log.Debug("Entrando al metodo: " + MethodBase.GetCurrentMethod().Name);
+                conector = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString);
+                conector.Open();
+                MySqlCommand comandoSql = conector.CreateCommand();
+                comandoSql.CommandText = string.Format("INSERT INTO asignatura (nombre, creditos, curso, cuatrimestre, idGrado, idProfesor, idTipoAsignatura) VALUES (@0, @1, @2, @3, @4, @5, @6);");
+                comandoSql.Parameters.Add(new MySqlParameter("0", nombre ));
+                comandoSql.Parameters.Add(new MySqlParameter("1", creditos));
+                comandoSql.Parameters.Add(new MySqlParameter("2", curso));
+                comandoSql.Parameters.Add(new MySqlParameter("3", cuatrimestre));
+                comandoSql.Parameters.Add(new MySqlParameter("4", idGrado));
+                comandoSql.Parameters.Add(new MySqlParameter("5", idProfesor));
+                comandoSql.Parameters.Add(new MySqlParameter("6", idTipoAsignatura));
+              
+                var verificador = comandoSql.ExecuteNonQuery();
+                respuesta.Status = 200;
+                respuesta.Afectados = verificador;
+                respuesta.Mensaje = "Insert a la base de datos exitoso!";
+            }
+            catch (Exception e)
+            {
+                log.Error("Error en la conexion a base de datos", e);
+                respuesta.Status = 500;
+                respuesta.Mensaje = "Error en la conexion a base de datos";
+            }
+            finally
+            {
+                if (conector != null)
+                {
+                    conector.Close();
+                    conector.Dispose();
+                }
+            }
+            log.Debug("Saliendo del metodo: " + MethodBase.GetCurrentMethod().Name);
+            return respuesta;
+        }
+
+        public RespuestaLecturaProfesores ConsultarProfesores()
+        {
+            RespuestaLecturaProfesores respuesta = new RespuestaLecturaProfesores();
+            MySqlConnection conector = null;
+            Profesores profesor;
+            ListaProfesores lista;
+            try
+            {
+                log.Debug("Entrando al metodo: " + MethodBase.GetCurrentMethod().Name);
+                conector = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString);
+                conector.Open();
+                MySqlCommand comandoSql = conector.CreateCommand();
+                comandoSql.CommandText = string.Format("SELECT A.idPersona, A.nombre, B.nombre FROM persona as A join profesor as C on A.idPersona = C.idPersona join asignatura as B on B.idProfesor = C.idProfesor;");
+                var verificador = comandoSql.ExecuteNonQuery();
+                MySqlDataReader lectorTablaSQL = comandoSql.ExecuteReader();
+                lista = new ListaProfesores();
+                Departamento departamento = null;
+                while (lectorTablaSQL.Read())
+                {
+                    profesor = new Profesores();
+                    profesor.Id = Convert.ToInt32(lectorTablaSQL.GetString(0));
+                    profesor.Nombre = lectorTablaSQL.GetString(1);
+                    profesor.Asignatura = lectorTablaSQL.GetString(2);
+                    lista.AgregarProfesor(profesor);
+                }
+                respuesta.Mensaje = "Lectura exitosa";
+                respuesta.Listado = lista;
+                respuesta.Status = 200;
+                //respuesta.Afectados = verificador;
+            }
+            catch (Exception e)
+            {
+                log.Error("Error en la conexion a base de datos", e);
+                respuesta.Status = 500;
+                respuesta.Mensaje = "Error en la conexion a base de datos";
+            }
+            finally
+            {
+                if (conector != null)
+                {
+                    conector.Close();
+                    conector.Dispose();
+                }
+            }
+            log.Debug("Saliendo del metodo: " + MethodBase.GetCurrentMethod().Name);
+            return respuesta;
+        }
+
+        public RespuestaEscritura ModificarDepartamento(int idDepartamento, string nuevo)
+        {
+            RespuestaEscritura respuesta = new RespuestaEscritura();
+            MySqlConnection conector = null;
+            try
+            {
+                log.Debug("Entrando al metodo: " + MethodBase.GetCurrentMethod().Name);
+                conector = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString);
+                conector.Open();
+                MySqlCommand comandoSql = conector.CreateCommand();
+                comandoSql.CommandText = string.Format("UPDATE departamento SET nombre = @0 WHERE idDepartamento = @1;");
+                comandoSql.Parameters.Add(new MySqlParameter("0", nuevo));
+                comandoSql.Parameters.Add(new MySqlParameter("1", idDepartamento));
+                int verificador = comandoSql.ExecuteNonQuery();
+                respuesta.Status = 200;
+                respuesta.Afectados = verificador;
+                if (verificador != 0){
+                    respuesta.Mensaje = "Modificacion a la base de datos exitosa!";
+                }else{
+                    respuesta.Mensaje = "Departamento inexitente";
+                }
+            }
+            catch (Exception e)
+            {
+                log.Error("Error en la conexion a base de datos", e);
+                respuesta.Status = 500;
+                respuesta.Mensaje = "Error en la conexion a base de datos";
+            }
+            finally
+            {
+                if (conector != null)
+                {
+                    conector.Close();
+                    conector.Dispose();
+                }
+            }
+            log.Debug("Saliendo del metodo: " + MethodBase.GetCurrentMethod().Name);
+            return respuesta;
+        }
+
+        public RespuestaEscritura EliminarDepartamento(int idDepartamento)
+        {
+            RespuestaEscritura respuesta = new RespuestaEscritura();
+            MySqlConnection conector = null;
+            try
+            {
+                log.Debug("Entrando al metodo: " + MethodBase.GetCurrentMethod().Name);
+                conector = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString);
+                conector.Open();
+                MySqlCommand comandoSql = conector.CreateCommand();
+                comandoSql.CommandText = string.Format("DELETE FROM departamento WHERE idDepartamento = @0;");
+                comandoSql.Parameters.Add(new MySqlParameter("0", idDepartamento));
+                int verificador = comandoSql.ExecuteNonQuery();
+                respuesta.Status = 200;
+                respuesta.Afectados = verificador;
+                respuesta.Mensaje = "Delete a la base de datos exitoso!";
+            }
+            catch (Exception e)
+            {
+                log.Error("Error en la conexion a base de datos", e);
+                respuesta.Status = 500;
+                respuesta.Mensaje = "Error en la conexion a base de datos";
+            }
+            finally
+            {
+                if (conector != null)
+                {
+                    conector.Close();
+                    conector.Dispose();
+                }
+            }
+            log.Debug("Saliendo del metodo: " + MethodBase.GetCurrentMethod().Name);
+            return respuesta;
+        }
+    }
+}
